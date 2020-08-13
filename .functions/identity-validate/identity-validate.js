37d8de6430c6b9b9e72105ce92f1718ca48af9c1
Raymond Camden <raymondcamden@gmail.com>
2020-08-13 17:07:12 -0500

    work5

--- .functions/identity-validate/identity-validate.js
+++ .functions/identity-validate/identity-validate.js
@@ -2,14 +2,44 @@
 exports.handler = async (event, context) => {
   console.log('identity-validate');
   console.log(event.body);
+
+  let user = event.body.user;
+  if(!user) {
+    console.log('no user??');
+    return {
+      statusCode: 500
+    }
+  }
+
+  let me = {
+    email:user.email, 
+    name:user.user_metadata.full_name;
+  }
+  console.log('me', JSON.stringify(me));
+
+  /*
+	var me = new user({
+		email:profile.emails[0].value,
+		name:profile.displayName
+	});
+
+	user.findOne({email:me.email}, function(err, u) {
+		if(!u) {
+			me.save(function(err, me) {
+				if(err) return done(err);
+				done(null,me);
+			});
+		} else {
+			//console.log(u);
+			done(null, u);
+		}
+	});
+  */
+
   try {
-    const subject = event.queryStringParameters.name || 'World'
     return {
       statusCode: 200,
-      body: JSON.stringify({ message: `Hello ${subject}` }),
-      // // more keys you can return:
-      // headers: { "headerName": "headerValue", ... },
-      // isBase64Encoded: true,
+      body: '',
     }
   } catch (err) {
     return { statusCode: 500, body: err.toString() }
